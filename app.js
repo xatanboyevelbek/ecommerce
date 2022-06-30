e9a3165db23e3b0913163b91f986a7408eebdee9
xatanboyevelbek <programmerelbek@gmail.com>
2022-06-30 16:46:49 +0500

    New features

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
