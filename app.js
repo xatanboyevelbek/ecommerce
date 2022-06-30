f83c1ab62d829a01b599a21745ce90f52c3bb65c
xatanboyevelbek <programmerelbek@gmail.com>
2022-06-30 16:52:26 +0500

    New features

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ const csrf = require('csurf');
 const flash = require('connect-flash');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static(path.join(__dirname + '/public')));
