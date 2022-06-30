de2d4f19587941c7b469256c9f6d7bb48517b5a7
xatanboyevelbek <programmerelbek@gmail.com>
2022-06-30 20:20:58 +0500

    New features

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ const flash = require('connect-flash');
 const ejs = require('ejs');
 const session = require('express-session');
 const mongoose = require('mongoose');
+const mongodbSession = require('connect-mongodb-session')(session);
 const app = express();
 const MONGODB_URI = 'mongodb+srv://elbekkhatanboyev:27092001Elbek@cluster0.ch1k2.mongodb.net/ecommerce?retryWrites=true&w=majority';
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(csrf());
+app.use(flash());
 
 mongoose.connect(MONGODB_URI).then(() => {
     app.listen(3000);
